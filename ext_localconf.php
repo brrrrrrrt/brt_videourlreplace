3deb21caa46282c7d912928c242de86fe8631b24
brrrrrrrt <brt@effet.info>
2015-10-04 19:52:33 +0200

    [BUGFIX] TYPO3 V7.5 compatibility issue

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-require_once t3lib_extMgm::extPath($_EXTKEY).'Classes/class.tx_brtvideourlreplace.php';
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/class.tx_brtvideourlreplace.php';
 
 // hook is called before Caching / pages on their way in the cache:
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'Brt\VideoUrlReplace\tx_brtvideourlreplace->replace';
